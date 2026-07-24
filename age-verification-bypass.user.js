45a257a7d1c5aa11d59e37bf3d284be712839bf8
LucianoSkx <LucianoSkx@users.noreply.github.com>
2026-07-24 20:49:48 -0300

    bump version to 1.4.1

--- age-verification-bypass.user.js
+++ age-verification-bypass.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Age Verification Bypass
 // @namespace    https://github.com/LucianoSkx/age-verification-bypass
-// @version      1.4.0
+// @version      1.4.1
 // @description  Age verification bypass userscript (port of helloyanis' Firefox add-on)
 // @author       helloyanis (original), LucianoSkx (port)
 // @match        *://*/*
